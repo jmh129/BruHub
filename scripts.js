5646810b52f7ef05f195f7500a47d364a3b19a6c
James Holding <james.holding90@gmail.com>
2020-06-18 13:45:12 -0400

    removed script.js file.

--- scripts.js
+++ scripts.js
@@ -29,4 +29,25 @@ $(document).ready(function () {
       }
     });
   }
+
+  //On click function for #button1
+  $("#button1").on("click", function () {
+    $("body").css("background-color", "white");
+    console.log("button1");
+  });
+  //On click function for #button2
+  $("#button2").on("click", function () {
+    $.ajax({
+      url: "https://official-joke-api.appspot.com/random_joke",
+      method: "get",
+    }).then(function (response) {
+        $("#joke").empty();
+      var jokeResponse = document.createElement("div");
+      $(jokeResponse).html(
+        response.setup + "<br><br><br><br>" + response.punchline
+      );
+      $(jokeResponse).attr("color", "white");
+      $("#joke").append(jokeResponse);
+    });
+  });
 });
