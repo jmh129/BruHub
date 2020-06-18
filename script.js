6324006ca719f2863051ce736f9541fffcb9d12c
James Holding <james.holding90@gmail.com>
2020-06-18 14:59:32 -0400

    updated li in css. fixed meme and joke functions.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
   searchCity();
   renderJoke();
+  renderMeme();
 
   function searchCity() {
     $("#search-btn").on("click", function (event) {
@@ -35,7 +36,6 @@ $(document).ready(function () {
     });
   }
 
-<<<<<<< HEAD
   //On click function for #button2 NEED TO REWORK/CLEANUP
   function renderJoke() {
     $("#button2").on("click", function () {
@@ -53,33 +53,19 @@ $(document).ready(function () {
       });
     });
   }
-=======
-  //On click function for #button2
-  $("#button1").on("click", function () {
-    $.ajax({
-      url: "https://meme-api.herokuapp.com/gimme",
-      method: "get",
-    }).then(function (response) {
-      $("#joke").empty();
-      var memeResponse = document.createElement("img");
-      $(memeResponse).attr("src",response.url);
-      $(memeResponse).attr("color", "white");
-      $("#joke").append(memeResponse);
+  // On click function for #button2
+  function renderMeme() {
+    $("#button1").on("click", function () {
+      $.ajax({
+        url: "https://meme-api.herokuapp.com/gimme",
+        method: "get",
+      }).then(function (response) {
+        $("#joke").empty();
+        var memeResponse = document.createElement("img");
+        $(memeResponse).attr("src", response.url);
+        $(memeResponse).attr("color", "white");
+        $("#joke").append(memeResponse);
+      });
     });
-  });
-
-  //On click function for #button2
-  $("#button2").on("click", function () {
-    $.ajax({
-      url: "https://official-joke-api.appspot.com/random_joke",
-      method: "get",
-    }).then(function (response) {
-      $("#joke").empty();
-      var jokeResponse = document.createElement("div");
-      $(jokeResponse).html(response.setup + "<br><br><br><br>" + response.punchline);
-      $(jokeResponse).attr("color", "white");
-      $("#joke").append(jokeResponse);
-    });
-  });
->>>>>>> 4561b859f9bfd50c3452286cc4ad1c34a6b3a9a7
+  }
 });
