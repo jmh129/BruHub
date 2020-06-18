e25261518eab25a53ac8c44e996a6bc66372e9fb
James Holding <james.holding90@gmail.com>
2020-06-18 12:01:39 -0400

    on click function added.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
   console.log("Document is Ready");
 
-  var bruHubURL = ("https://api.openbrewerydb.org/breweries?by_city=");
-  
+  //   var bruHubURL = "https://api.openbrewerydb.org/breweries?by_city=";
 
-  $.ajax({
-    url: bruHubURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-  });
+  //   $.ajax({
+  //     url: bruHubURL,
+  //     method: "GET",
+  //   }).then(function (response) {
+  //     console.log(response);
+  //   });
+
+  // receive search input and converts it to a variable that can be used in query
+  // queries need their spaces changed to underscores to work
+  function searchCity() {
+    $("#search-btn").on("click", function (event) {
+      event.preventDefault();
+    //   console.log("button was clicked.");
+      var cityInput = $("#search-text").val();
+    //   console.log(cityInput);
+    });
+  }
 });
-
-// receive search input and converts it to a variable that can be used in query 
-// queries need their spaces changed to underscores to work
-function searchInput() {
-$("#search-btn").on("click", function () {
-    event.preventDefault();
-    var cityInput = $("#search-text").val();
-    console.log(searchCity);
-})};
