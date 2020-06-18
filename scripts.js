3d1893006d70ee7216297802dcf5bdc5f05b406a
James Holding <james.holding90@gmail.com>
2020-06-18 10:48:06 -0400

    openbrewerydb api call successfull.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
- $(document).ready(function() {
-     console.log("Document is Ready");
- });
+$(document).ready(function () {
+  console.log("Document is Ready");
 
-// sandbox URL: https://sandbox-api.brewerydb.com/v2/
-// sandbox APIKey: c65b561c1cee41ca19243c8bc236219c
+  var bruHubURL = ("https://api.openbrewerydb.org/breweries/search?");
+  
+  $.ajax({
+    url: bruHubURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+});
