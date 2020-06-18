91b03dded3fbbd7e076dca1810a3c3d47348eb4d
James Holding <james.holding90@gmail.com>
2020-06-18 09:33:31 -0400

    added jquery minified to HTML- JS File document ready.

---
+++ scripts.js
@@ -0,0 +1,6 @@
+ $(document).ready(function() {
+     console.log("Document is Ready");
+ });
+
+// sandbox URL: https://sandbox-api.brewerydb.com/v2/
+// sandbox APIKey: c65b561c1cee41ca19243c8bc236219c
