f03e80d01e10f89ab281c5626f218acd47c0aaf7
imac <chadmdubay@gmail.com>
2020-06-18 13:16:13 -0400

    Centered joke button and response.

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-
-    $.ajax({
-        url: "https://api.openbrewerydb.org/breweries/search?query=atlanta&city=atlanta",
-        method: "get"
-    }).then(function(response){
-    console.log(response);
-    })
-
-    
-
     //On click function for #button1
     $("#button1").on("click",function(){
         $("body").css("background-color","white");
@@ -20,12 +10,10 @@ $(document).ready(function(){
             url: "https://official-joke-api.appspot.com/random_joke",
             method: "get"
         }).then(function(response){
-            console.log(response);
-        var jokeResponse = document.createElement("div");
-        $(jokeResponse).html(response.setup+response.punchline);
-        $(jokeResponse).attr("color","white");
-        $("#joke").append(jokeResponse);
+            var jokeResponse = document.createElement("div");
+            $(jokeResponse).html(response.setup+"<br><br><br><br>"+response.punchline);
+            $(jokeResponse).attr("color","white");
+            $("#joke").append(jokeResponse);
         })
     })
-
 })
