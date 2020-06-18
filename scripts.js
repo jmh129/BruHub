f4062c4d7cf2eccf8475b0b821c43650acb7a9b1
James Holding <james.holding90@gmail.com>
2020-06-18 12:27:24 -0400

    OpenBrewAPI is not returning results from search input.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
 $(document).ready(function () {
   console.log("Document is Ready");
 
-  //   var bruHubURL = "https://api.openbrewerydb.org/breweries?by_city=";
+  var cityInput;
 
-  //   $.ajax({
-  //     url: bruHubURL,
-  //     method: "GET",
-  //   }).then(function (response) {
-  //     console.log(response);
-  //   });
+  searchCity();
 
-  // receive search input and converts it to a variable that can be used in query
-  // queries need their spaces changed to underscores to work
   function searchCity() {
     $("#search-btn").on("click", function (event) {
       event.preventDefault();
-    //   console.log("button was clicked.");
-      var cityInput = $("#search-text").val();
-    //   console.log(cityInput);
+      cityInput = $("#search-text").val();
+      runOpenBrewAPI();
+    });
+  }
+
+  function runOpenBrewAPI() {
+    var OpenBrewAPIURL =
+      "https://api.openbrewerydb.org/breweries?by_city=" + cityInput;
+
+    $.ajax({
+      url: OpenBrewAPIURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    //   Need to create function that converts citys with spaces to have _ 
     });
   }
 });
