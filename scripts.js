34a6d58b4b1e53388dc2c246f92a9094850bd705
James Holding <james.holding90@gmail.com>
2020-06-18 11:22:58 -0400

    base function added to log search input.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   console.log("Document is Ready");
 
-  var bruHubURL = ("https://api.openbrewerydb.org/breweries/search?");
+  var bruHubURL = ("https://api.openbrewerydb.org/breweries?by_city=");
   
+
   $.ajax({
     url: bruHubURL,
     method: "GET",
@@ -10,3 +11,12 @@ $(document).ready(function () {
     console.log(response);
   });
 });
+
+// receive search input and converts it to a variable that can be used in query 
+// queries need their spaces changed to underscores to work
+function searchInput() {
+$("#search-btn").on("click", function () {
+    event.preventDefault();
+    var searchCity = $("#search-text").val();
+    console.log(searchCity);
+})};
